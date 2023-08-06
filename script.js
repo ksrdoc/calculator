a280b6a213780e9aa535524a70eb05d0402fd0a1
ksrdoc <karlo.srdoc2@gmail.com>
2023-08-06 16:49:08 +0200

    Build the basic operating functions

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Operations
+
+const addition = function (a, b) {
+  return a + b;
+};
+
+const substraction = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const division = function (a, b) {
+  return a / b;
+};
