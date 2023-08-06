951dbdd44a67927b6396e5f13d40c04883824f54
ksrdoc <karlo.srdoc2@gmail.com>
2023-08-06 17:14:02 +0200

    created the operate function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+//Operator variables
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
 // Operations
 
 const addition = function (a, b) {
@@ -16,4 +22,22 @@ const multiply = function (a, b) {
 
 const division = function (a, b) {
   return a / b;
+};
+
+const operate = function (firstNumber, operator, secondNumber) {
+  a = Number(firstNumber);
+  b = Number(secondNumber);
+  switch (operator) {
+    case "+":
+      return addition(a, b);
+    case "-":
+      return substraction(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) return null;
+      else return division(a, b);
+    default:
+      return null;
+  }
 };
