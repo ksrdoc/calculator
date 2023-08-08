26cc4911558488bbbadca65e3467d00a00c1c625
ksrdoc <karlo.srdoc2@gmail.com>
2023-08-08 17:46:47 +0200

    Built add and subtract features

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+let localSum = "";
 
 // Operations
 
@@ -25,8 +26,8 @@ const division = function (a, b) {
 };
 
 const operate = function (firstNumber, operator, secondNumber) {
-  a = Number(firstNumber);
-  b = Number(secondNumber);
+  let a = Number(firstNumber);
+  let b = Number(secondNumber);
   switch (operator) {
     case "+":
       return addition(a, b);
@@ -45,7 +46,7 @@ const operate = function (firstNumber, operator, secondNumber) {
 //Functions
 
 const appendNumber = function () {
-  result.textContent += this.textContent;
+  result.textContent += Number(this.textContent);
 };
 
 const appendDot = function () {
@@ -58,6 +59,10 @@ const appendDot = function () {
 
 const allClearFoo = function () {
   result.textContent = "";
+  lastResult.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
 };
 
 const clearFoo = function () {
@@ -78,8 +83,12 @@ const numberSeven = document.querySelector("#numSeven");
 const numberEight = document.querySelector("#numEight");
 const numberNine = document.querySelector("#numNine");
 const result = document.querySelector(".screen-result");
+const lastResult = document.querySelector(".screen-last");
 const allClear = document.querySelector("#allClear");
 const clear = document.querySelector("#clear");
+const addKey = document.querySelector("#addition");
+const minusKey = document.querySelector("#subtraction");
+const equalKey = document.querySelector("#equal");
 
 //Event listeners
 
@@ -96,5 +105,50 @@ numberNine.addEventListener("click", appendNumber);
 dot.addEventListener("click", appendDot);
 allClear.addEventListener("click", allClearFoo);
 clear.addEventListener("click", clearFoo);
+addKey.addEventListener("click", function () {
+  if (lastResult.textContent === "") {
+    // this can be put in a function no prob
+    lastResult.textContent = `${result.textContent} ${this.textContent}`;
+    firstNumber = Number(result.textContent);
+    result.textContent = "";
+    operator = this.textContent;
+  } //else {
+  //lastResult.textContent = "";
+  // secondNumber = Number(result.textContent);
+  //lastResult.textContent = operate(
+  //   firstNumber,
+  //   this.textContent,
+  //   secondNumber
+  //);
+  // localSum = Number(lastResult.textContent);
+  // console.log(secondNumber);
+  // operator = this.textContent;
+  // result.textContent = "";
+  // result.textContent = Number(localSum);
+  // secondNumber = "";
+  // firstNumber = Number(localSum);
+  //}
+});
+minusKey.addEventListener("click", function () {
+  if (lastResult.textContent === "") {
+    lastResult.textContent = `${result.textContent} ${this.textContent}`;
+    firstNumber = Number(result.textContent);
+    result.textContent = "";
+    operator = this.textContent;
+  }
+});
+
+equalKey.addEventListener("click", function () {
+  if (lastResult.textContent.includes("=") === true) {
+    console.log("Calculation is done");
+  } else {
+    secondNumber = Number(result.textContent);
+    lastResult.textContent += ` ${secondNumber} =`;
+    result.textContent = "";
+    result.textContent = operate(firstNumber, operator, secondNumber);
+    firstNumber = Number(result.textContent);
+    secondNumber = "";
+  }
+});
 
 // start implementing functions for operators
